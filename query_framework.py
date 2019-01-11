3346a8011ce7c1d0552c45e7be0bba1e08799441
Malyaj Srivastav <Malyaj@users.noreply.github.com>
2019-01-11 15:24:13 +0530

    Create query_framework.py
    Provides a framework to write the SQL query in a text file, and execute it in the python script

---
+++ query_framework.py
@@ -0,0 +1,43 @@
+import os
+import sqlite3
+
+## making a framework in which SQL query is written in a text file,
+## and then imported and excuted in this python script
+
+
+path = r"D:\Users\703143501\Documents\Genpact Internal\H\learning_sql\Jan_2019"
+os.chdir(path)
+
+
+### connecting to the database
+db = 'tables.db'
+conn = sqlite3.connect(db)
+
+
+### importing the query from the text file
+query_file = 'query.txt'
+query = ''
+
+for each in open(query_file, 'r'):
+    query = query + each
+
+newline = '\n'
+query = " ".join(query.split(newline))
+
+
+### executing the query
+cursor = conn.execute(query)
+
+### printing out some information about the query
+print("Query Executed:")
+print(query)
+print("-" * 100)
+
+### printing out the result
+print("Query Result:")
+for each in cursor:
+    print(each)
+
+
+### closing the connection to the database
+conn.close()
